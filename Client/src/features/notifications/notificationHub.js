49b14f27a3c755b1ede02e3e18c79dfdaa0b1ad5
Vo Minh Huy <huyvodtan@gmail.com>
2025-06-05 18:20:52 +0700

    test https

--- Client/src/features/notifications/notificationHub.js
+++ Client/src/features/notifications/notificationHub.js
@@ -1,7 +1,10 @@
 import * as signalR from '@microsoft/signalr';
+import { ServiceRoutes } from './../../services/ServiceRoutes';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const notificationHub = new signalR.HubConnectionBuilder()
-    .withUrl("https://localhost:5055/hubs/notification", {
+    .withUrl(`${API_BASE_URL}${ServiceRoutes.notification}/hubs/notification`, {
           withCredentials: true
     })
     .withAutomaticReconnect()
